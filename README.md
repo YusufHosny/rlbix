8818416c533d078b16a1c86168607671ceac49f6
Yusuf Hosny <yusufyy2004@hotmail.com>
2025-05-30 03:12:00 +0200

    added readme

---
+++ README.md
@@ -0,0 +1,2 @@
+# RLBIX
+Some small experiments with building a deep Q-learning RL model to learn to solve a rubik's cube. The model was able to learn to consistently solve a cube up to 4 shuffles away from solved with quite limited resources, and can be adjusted further to reach even better results. Built in python with pytorch.
--- training/model.py
+++ training/model.py
@@ -1,13 +1,13 @@
 import torch
 from torch import nn
 
-d_c = 6
-d_e = 100
-d_f = 50
-d_o = 50
-
-k_d = 3
-k_d3 = k_d**3
+d_c = 6 # n of channels
+d_fc1 = 100 # fc1 layers dimensions
+d_f = 50 # filter count for conv layers
+d_fco = 100 # fc2 layers dimensions
+k_d = 2 # conv kernel dim
+d_co = 2 # output dimension of conv layers (d_co * d_co * d_co)
+d_co3 = d_co ** 3 # dim after flattening
 
 class DQN(nn.Module):
 
@@ -15,32 +15,38 @@ class DQN(nn.Module):
         super(DQN, self).__init__()
         
         self.fc1 = nn.Sequential(
-            nn.Linear(d_c, d_e),
+            nn.Linear(d_c, d_fc1),
             nn.ELU(),
-            nn.Linear(d_e, d_e),
+            nn.Linear(d_fc1, d_fc1),
             nn.ELU(),
         )
 
         self.cnn = nn.Sequential(
-            nn.Conv3d(d_e, d_f, k_d, padding='valid'),
+            nn.Conv3d(d_fc1, d_f, k_d, padding='valid'),
+            nn.ELU(),
+            nn.Conv3d(d_f, d_f, k_d, padding='valid'),
+            nn.ELU(),
+            nn.Conv3d(d_f, d_f, k_d, padding='valid'),
             nn.ELU(),
         )
         self.fc2 = nn.Sequential(
-            nn.Linear(k_d3*d_f, d_o),
+            nn.Linear(d_co3*d_f, d_fco),
             nn.ELU(),
-            nn.Linear(d_o, d_o),
+            nn.Linear(d_fco, d_fco),
+            nn.ELU(),
+            nn.Linear(d_fco, d_fco),
             nn.ELU(),
         )
-        self.output = nn.Linear(d_o, n_actions)
+        self.output = nn.Linear(d_fco, n_actions)
 
     def forward(self, x):
         # [B, 6, 5, 5, 5] permute-> [B, 5, 5, 5, 6] fc1-> [B, 5, 5, 5, d_e] permute-> [B, d_e, 5, 5, 5]
-        # conv3d-> [B, d_f, k_d, k_d, k_d] flatten-> [B, k_d^3 *d_f] fc2-> [B, d_o] out-> [B, 12] -> out
+        # conv3d-> [B, d_f, d_co, d_co, d_co] flatten-> [B, d_co3 *d_f] fc2-> [B, d_fco] out-> [B, 12] -> out
         x = torch.permute(x, (0, 2, 3, 4, 1))
         x = self.fc1(x)
         x = torch.permute(x, (0, 4, 1, 2, 3))
         x = self.cnn(x)
-        x = x.reshape(-1, k_d3 *d_f)
+        x = x.reshape(-1, d_co3 *d_f)
         x = self.fc2(x)
         x = self.output(x)
         return x
